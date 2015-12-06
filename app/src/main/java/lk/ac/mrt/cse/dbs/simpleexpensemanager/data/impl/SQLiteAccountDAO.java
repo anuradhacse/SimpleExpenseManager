5cd62fb1c8939c359cda09b403e742d03f2f1f1d
Nirosh <niroshonmora@gmail.com>
2015-12-06 18:11:37 +0530

    database handling complete

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/SQLiteAccountDAO.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/SQLiteAccountDAO.java
@@ -4,12 +4,14 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.DBHandling.DBHandle;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
@@ -17,121 +19,151 @@ import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
 /**
  * Created by Anuradha Niroshan on 03/12/2015.
  */
-public class SQLiteAccountDAO extends SQLiteOpenHelper implements AccountDAO{
+public class SQLiteAccountDAO  implements AccountDAO{
 
-    // Database Name
-    private static final String DATABASE_NAME = "130227F";
-    private static final int DATABASE_VERSION = 1;
-
-    public static final String TABLE_NAME = "Account";
-    public static final String ACCOUNT_NO = "account_no";
-    public static final String BANK = "bank";
-    public static final String BANK_HOLDER = "holder";
-    public static final String INITIAL_BALANCE = "initial_balance";
-
-
-    public SQLiteAccountDAO(Context context){
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-
-    }
 
     @Override
     public List<String> getAccountNumbersList() {
-        List<String> accoutNumberList = new ArrayList<>();
-        String selectQuery = "SELECT "+ACCOUNT_NO+" FROM "+TABLE_NAME;
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
+        ArrayList<String> accountNumbers = new ArrayList<>();
 
-        if (cursor.moveToFirst()) {
-            do {
-                accoutNumberList.add(cursor.getString(0));
-            } while (cursor.moveToNext());
+        SQLiteDatabase db = null;
+        Cursor cursor = null;
+        try {
+            db = DBHandle.getWritableDatabase();
+
+            cursor = db.query(DBHandle.ACCOUNT_TABLE, new String[]{DBHandle.ACCOUNT_NO}, null, null, null, null, null); // Get account numbers from the database
+            if (cursor.moveToFirst()) {
+                do {
+
+                    accountNumbers.add(cursor.getString(0));
+                } while (cursor.moveToNext());
+            }
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            if (db != null) {
+                db.close();
+            }
         }
-        // return accoutNumtList list
-        db.close();
-        return accoutNumberList;
+        return accountNumbers;
     }
 
     @Override
     public List<Account> getAccountsList() {
-        List<Account> accountList = new ArrayList<>();
+        ArrayList<Account> accountList = new ArrayList<>();
+        SQLiteDatabase db = null;
+        Cursor cursor = null;
+        try {
+            db = DBHandle.getWritableDatabase();
 
-        String selectQuery = "SELECT "+"*"+" FROM "+TABLE_NAME;
+            cursor = db.query(DBHandle.ACCOUNT_TABLE, null, null, null, null, null, null);// Get all accounts from the database
+            if (cursor.moveToFirst()) {
+                do {
 
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
-
-        if (cursor.moveToFirst()) {
-            do {
-                Account account=new Account(null,null,null,0);
-                account.setAccountNo(cursor.getString(0));
-                account.setBankName(cursor.getString(1));
-                account.setAccountHolderName(cursor.getString(2));
-                account.setBalance(cursor.getDouble(3));
-                accountList.add(account);
-            } while (cursor.moveToNext());
+                    Account account = new Account(cursor.getString(0), cursor.getString(1), cursor.getString(2), Double.parseDouble(cursor.getString(3)));
+                    accountList.add(account);
+                } while (cursor.moveToNext());
+            }
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            if (db != null) {
+                db.close();
+            }
         }
-        // return accountList list
-        db.close();
         return accountList;
     }
 
     @Override
     public Account getAccount(String accountNo) throws InvalidAccountException {
-        String selectQuery = "SELECT "+BANK_HOLDER+" FROM "+TABLE_NAME+" WHERE "+ACCOUNT_NO+"="+accountNo;
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
+        SQLiteDatabase db = null;
+        Cursor cursor = null;
+        try {
+            db = DBHandle.getWritableDatabase();
 
-        if (cursor.moveToFirst()) {
-            do {
-                Account account=new Account(null,null,null,0);
-                account.setAccountNo(cursor.getString(0));
-                account.setBankName(cursor.getString(1));
-                account.setAccountHolderName(cursor.getString(2));
-                account.setBalance(cursor.getDouble(3));
+            cursor = db.query(DBHandle.ACCOUNT_TABLE, null, DBHandle.ACCOUNT_NO + " = ?", new String[]{accountNo}, null, null, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+
+                return new Account(cursor.getString(0), cursor.getString(1), cursor.getString(2), Double.parseDouble(cursor.getString(3)));
+
+            } else {
+                String msg = "Not a valid account ";
+                throw new InvalidAccountException(msg);
+            }
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+            if (db != null) {
                 db.close();
-                return account;
-            } while (cursor.moveToNext());
+            }
         }
         return null;
     }
 
     @Override
     public void addAccount(Account account) {
-        SQLiteDatabase db=this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(ACCOUNT_NO, account.getAccountNo());
-        values.put(BANK, account.getBankName());
-        values.put(BANK_HOLDER, account.getAccountHolderName());
-        values.put(INITIAL_BALANCE, account.getBalance());
-        db.insert(TABLE_NAME, null, values);
-        values.clear();
-        db.close(); // Closing database connection
+        SQLiteDatabase db = null;
+
+        try {
+            db = DBHandle.getWritableDatabase();
+
+            ContentValues values = new ContentValues();
+            values.put(DBHandle.ACCOUNT_NO, account.getAccountNo()); // account number
+            values.put(DBHandle.BANK_NAME, account.getBankName()); // bank name
+            values.put(DBHandle.HOLDER_NAME, account.getAccountHolderName()); // holder name
+            values.put(DBHandle.BALANCE, account.getBalance()); // initial balance
+
+            // Inserting Row
+            db.insert(DBHandle.ACCOUNT_TABLE, null, values);
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        } finally {
+            if (db != null) {
+                db.close();
+            }
+        }
     }
 
     @Override
     public void removeAccount(String accountNo) throws InvalidAccountException {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_NAME, ACCOUNT_NO + " = ?",
-                new String[] { String.valueOf(accountNo) });
+        SQLiteDatabase db=null;
+        try{
+            db = DBHandle.getWritableDatabase();
+            db.delete(DBHandle.ACCOUNT_TABLE, DBHandle.ACCOUNT_NO + " = ?",
+                    new String[]{String.valueOf(accountNo)});
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        }
         db.close();
     }
 
     @Override
     public void updateBalance(String accountNo, ExpenseType expenseType, double amount) throws InvalidAccountException {
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db=null;
+        Account account=getAccount(accountNo);
+        db = DBHandle.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+
+        switch (expenseType){
+            case EXPENSE:
+                contentValues.put(DBHandle.BALANCE,account.getBalance()-amount);
+                break;
+            case INCOME:
+                contentValues.put(DBHandle.BALANCE,account.getBalance()+amount);
+                break;
+        }
+        db.update(DBHandle.ACCOUNT_TABLE,contentValues,DBHandle.ACCOUNT_NO+"= ?",new String[]{accountNo});
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        String MYdatabase = "CREATE TABLE " +TABLE_NAME+ " ("
-                + ACCOUNT_NO + " TEXT PRIMARY KEY, " +BANK+ " TEXT, "
-                +BANK_HOLDER+ " TEXT," +INITIAL_BALANCE+" DOUBLE"+ ");";
-        db.execSQL(MYdatabase);
-    }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-    }
 }
